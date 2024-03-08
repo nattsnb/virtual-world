f7c3c234648694d8ae694d374887a531cd20d865
Natasza ASzatan <natiye.nb@gmai.com>
2024-03-08 07:24:41 +0000

    creted for loop nested in anothrer for loop to create all tiles on board. tiles have x-i and y-j classes that allows to address them

--- src/board.js
+++ src/board.js
@@ -5,13 +5,17 @@ export class Board {
         this.render()
     }
     render(){
-        const boardContainer = document.querySelector("#board-container")
+        const boardContainer = document.querySelector("#board-container");
         for (let i = 0; i < this.width; i++){
-            const tile = document.createElement("div")
-            tile.classList.add(`column-${i}`)
-            boardContainer.append(tile)
+            const column = document.createElement("div")
+            column.classList.add(`column-${i}`);
+            boardContainer.append(column);
+            for (let j = 0; j < this.height; j++) {
+                const tile = document.createElement("div")
+                tile.classList.add(`x-${i}`, `y-${j}`)
+                column.append(tile)
+            }
         }
-
     }
 }
 
