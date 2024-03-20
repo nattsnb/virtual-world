c592a533263c5a4793505fb5fb64327eb6276151
Natasza ASzatan <natiye.nb@gmai.com>
2024-03-20 15:20:52 +0000

    board render() creates rows and adds tile object inside. dunno how to add tile as div not object.

--- src/board.js
+++ src/board.js
@@ -1,45 +1,44 @@
 import {organismsList} from './organismsList.js';
 import {Player} from "./player";
+import {Tile} from "./tile.js"
 
 export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.tiles = [];
     this.render();
   }
 
   render() {
-    // const boardContainer = document.querySelector('#board-container');
-    let boardContainer;
-    boardContainer = [];
+    const boardContainer = document.querySelector('#board-container');
     for (let i = 0; i < this.width; i++) {
-      boardContainer[i] = [];
+      this.tiles[i] = [];
     }
     for (let i = 0; i < this.width; i++) {
-      // const column = document.createElement('div');
-      // column.classList.add(`column-${i}`);
-      // boardContainer.append(column);
+      const row = document.createElement('div');
+      row.classList.add(`row`);
+      boardContainer.append(row);
       for (let j = 0; j < this.height; j++) {
-        boardContainer[i][j] = i + ', ' + j;
-        // const tile = document.createElement('div');
-        // tile.classList.add(`x-${i}`, `y-${j}`);
-        // column.append(tile);
+        this.tiles[i][j] = i + ', ' + j;
+        const tile = new Tile(i,j);
+        row.append(tile);
       }
     }
-    console.log(boardContainer)
+    console.log(this.tiles)
   }
-  createInitialCharacters() {
-    let numberOfCharacters = Math.round(this.width * this.height * 0.3);
-    for (numberOfCharacters; numberOfCharacters > 0; numberOfCharacters--) {
-      let organism = new organismsList[Math.floor(Math.random() * organismsList.length)]();
-      console.log("organism: " + organism.name);
-      const randomWidth = Math.floor(Math.random() * this.width);
-      const randomHeight = Math.floor(Math.random() * this.height);
-      console.log("x: " + randomWidth + " y: " + randomHeight)
-      let tileForNewOrganism = document.querySelector('.x-${randomWidth}.y-${randomHeight}');
-      tileForNewOrganism.innerText = "lalala"
-    }
-    const player = new Player();
-    console.log("organism: " + player.name);
-    }
+  // createInitialCharacters() {
+  //   let numberOfCharacters = Math.round(this.width * this.height * 0.3);
+  //   for (numberOfCharacters; numberOfCharacters > 0; numberOfCharacters--) {
+  //     let organism = new organismsList[Math.floor(Math.random() * organismsList.length)]();
+  //     console.log("organism: " + organism.name);
+  //     const randomWidth = Math.floor(Math.random() * this.width);
+  //     const randomHeight = Math.floor(Math.random() * this.height);
+  //     console.log("x: " + randomWidth + " y: " + randomHeight)
+  //     let tileForNewOrganism = document.querySelector('.x-${randomWidth}.y-${randomHeight}');
+  //     tileForNewOrganism.innerText = "lalala"
+  //   }
+  //   const player = new Player();
+  //   console.log("organism: " + player.name);
+  //   }
 }
--- src/tile.js
+++ src/tile.js
@@ -2,5 +2,11 @@ export class Tile {
     constructor (x,y) {
         this.x = x;
         this.y = y;
+        this.render()
+    }
+    render(){
+        const tileContainer = document.createElement('div');
+        tileContainer.classList.add('tileContainer');
+
     }
 }
