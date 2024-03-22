99a9ca31d46f2d82c0c5c7acfb791b070ffde8c8
Natasza ASzatan <natiye.nb@gmai.com>
2024-03-22 15:01:01 +0000

    edited the images, added image for antelope icon, centered the images

---
+++ src/antelope/Antelope.js
@@ -0,0 +1,16 @@
+import {Animal} from '../animal.js';
+import antelopeImage from "../antelope/antelope.jpg";
+
+export class Antelope extends Animal {
+    constructor() {
+        super();
+        this.initiative = 4;
+        this.strenght = 4;
+        this.createElement()
+    }
+
+    createElement() {
+        super.createElement()
+        this.element.src = antelopeImage;
+    }
+}
