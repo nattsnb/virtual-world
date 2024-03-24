6175690692935e33955bc87c132eb1261f988b12
Natasza ASzatan <natiye.nb@gmai.com>
2024-03-24 16:50:09 +0000

    fixed issues higlighted in PR

--- src/findRandomTile.js
+++ src/findRandomTile.js
@@ -4,7 +4,7 @@ export function findRandomTile (width, height, tiles) {
     const randomX = Math.floor(Math.random() * width);
     const randomY = Math.floor(Math.random() * height);
     const foundTile = tiles[randomX][randomY];
-    if (isTileEmpty(foundTile) !== true) {
+    if (!isTileEmpty(foundTile)) {
         return findRandomTile(width, height, tiles);
     }
     return foundTile
