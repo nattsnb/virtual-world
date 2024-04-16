dca8fc6b36c6c95b4b6aad5b1ea67e987392815f
Natasza ASzatan <natiye.nb@gmai.com>
2024-04-16 16:04:32 +0100

    developing mate function, if the same organisms meet, array of surrounding tiles is created

--- src/Animal.js
+++ src/Animal.js
@@ -8,7 +8,42 @@ export class Animal extends Organism {
     this.numberOfSteps = 1;
   }
 
-  mate() {}
+  mate(newTile, organism) {
+    console.log(organism.constructor.name)
+    if (newTile.currentOrganism !== null){
+      console.log(newTile.currentOrganism.constructor.name)
+      if(organism.constructor.name === newTile.currentOrganism.constructor.name){
+        console.log("it's a match!")
+        const parent1SurroundingTiles = findNearestTiles(
+            this.board.tiles,
+            organism,
+            this.board.width,
+            this.board.height,
+            this.numberOfSteps
+        );
+        console.log(parent1SurroundingTiles)
+        const parent2SurroundingTiles = findNearestTiles(
+            this.board.tiles,
+            newTile.currentOrganism,
+            this.board.width,
+            this.board.height,
+            this.numberOfSteps
+        );
+        let surroundingTiles = []
+        for(let i = 0; i < parent1SurroundingTiles.length; i++) {
+          surroundingTiles.push(parent1SurroundingTiles[i])
+        }
+        for(let i = 0; i < parent2SurroundingTiles.length; i++){
+          if (!surroundingTiles.includes(parent2SurroundingTiles[i])){
+            surroundingTiles.push(parent2SurroundingTiles[i]);
+          }
+        }
+        console.log(parent2SurroundingTiles)
+        console.log(surroundingTiles)
+
+      }
+    }
+  }
 
   fight() {}
 
@@ -27,6 +62,7 @@ export class Animal extends Organism {
     );
     const randomTileNumber = Math.floor(Math.random() * nearestTiles.length);
     const newTile = nearestTiles[randomTileNumber];
+    this.mate(newTile, this)
     console.log(`from: ${organism.x}, ${organism.y}`);
     newTile.addOrganism(organism);
     console.log(`to ${organism.x}, ${organism.y}`);
