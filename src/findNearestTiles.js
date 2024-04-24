69017f776502d3fd91233dfb341caf5f30ea8400
Natasza ASzatan <natiye.nb@gmai.com>
2024-04-24 12:38:45 +0100

    added description in findNearestTiles

--- src/findNearestTiles.js
+++ src/findNearestTiles.js
@@ -5,29 +5,54 @@ export function findNearestTiles(tiles, organism, width, height, steps) {
   const xMax = width - steps;
   const yMax = height - steps;
   let arrayOfNearestTiles = [];
+  // move left.up
   if (x > minimalStep && y > minimalStep) {
     arrayOfNearestTiles.push(tiles[x - steps][y - steps]);
   }
-  if (y > minimalStep) {
+ //move straight.up
+  if (y >= minimalStep) {
     arrayOfNearestTiles.push(tiles[x][y - steps]);
   }
+  // move right.up
   if (x < xMax && y > minimalStep) {
     arrayOfNearestTiles.push(tiles[x + steps][y - steps]);
   }
+  // move right.straight
   if (x < xMax) {
     arrayOfNearestTiles.push(tiles[x + steps][y]);
   }
+  // move right.down
   if (x < xMax && y < yMax) {
     arrayOfNearestTiles.push(tiles[x + steps][y + steps]);
   }
+  //move straight.down
   if (y < yMax) {
     arrayOfNearestTiles.push(tiles[x][y + steps]);
   }
+  // move left.down
   if (x > minimalStep && y < yMax) {
     arrayOfNearestTiles.push(tiles[x - steps][y + steps]);
   }
+  // move left.straight
   if (x > minimalStep) {
     arrayOfNearestTiles.push(tiles[x - steps][y]);
   }
   return arrayOfNearestTiles;
 }
+
+// const possibleSteps = {
+//   left: {
+//     up: tiles[x - steps][y - steps],
+//     down: tiles[x - steps][y + steps],
+//     straight: tiles[x - steps][y]
+//   },
+//   right: {
+//     up: tiles[x + steps][y - steps],
+//     down: tiles[x + steps][y + steps],
+//     straight: tiles[x + steps][y]
+//   },
+//   straight: {
+//     up: tiles[x][y - steps],
+//     down: tiles[x][y + steps],
+//   }
+// }
