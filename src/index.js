c7b10cbaf63336d4768dd2a2fb5ef1966005e6fe
Natasza ASzatan <natiye.nb@gmai.com>
2024-04-24 18:40:55 +0100

    fixed RWD

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './styles.css';
 import { Board } from './Board';
 
-const paragraph = document.createElement('p');
-paragraph.innerText = 'Hello!';
-document.body.append(paragraph);
-
 const board = new Board(10, 10);
 
 // for (let i = 0; i<20 ; i++) {
