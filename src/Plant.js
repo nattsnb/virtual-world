38ae9b3778bc3137264745995596a57723561be6
Natasza ASzatan <natiye.nb@gmai.com>
2024-04-26 16:22:26 +0100

    added findNearestTilesForPlants methode in Plant, it doesnt use numberOfSteps, so plants can use it

--- src/Plant.js
+++ src/Plant.js
@@ -16,7 +16,7 @@ export class Plant extends Organism {
     // console.log(odds)
     // console.log(this.chancesToSpread)
     if (odds < this.chancesToSpread) {
-      const parentSurroundingTiles = this.board.findNearestTiles(this);
+      const parentSurroundingTiles = this.findNearestTilesForPlant(this);
       // console.log(parentSurroundingTiles)
       let surroundingEmptyTiles = [];
       for (let i = 0; i < parentSurroundingTiles.length; i++) {
@@ -43,4 +43,52 @@ export class Plant extends Organism {
     this.board.tiles[this.x][this.y].setOrganism(organism);
     this.board.tiles[this.x][this.y].refresh();
   }
+  findNearestTilesForPlant = (plant) => {
+    const x = plant.x;
+    const y = plant.y;
+    const xMax = this.width - 1;
+    const yMax = this.height - 1;
+    let arrayOfNearestTiles = [];
+    // move left.up
+    if (x > 0 && y > 0) {
+      arrayOfNearestTiles.push(
+          this.board.tiles[x - 1][y - 1],
+      );
+    }
+    //move straight.up
+    if (y > 0) {
+      arrayOfNearestTiles.push(this.board.tiles[x][y - 1]);
+    }
+    // move right.up
+    if (x < xMax && y > 0) {
+      arrayOfNearestTiles.push(
+          this.board.tiles[x + 1][y - 1],
+      );
+    }
+    // move right.straight
+    if (x < xMax) {
+      arrayOfNearestTiles.push(this.board.tiles[x + 1][y]);
+    }
+    // move right.down
+    if (x < xMax && y < yMax) {
+      arrayOfNearestTiles.push(
+          this.board.tiles[x + 1][y + 1],
+      );
+    }
+    //move straight.down
+    if (y < yMax) {
+      arrayOfNearestTiles.push(this.board.tiles[x][y + 1]);
+    }
+    // move left.down
+    if (x > 0 && y < yMax) {
+      arrayOfNearestTiles.push(
+          this.board.tiles[x - 1][y + 1],
+      );
+    }
+    // move left.straight
+    if (x > 0) {
+      arrayOfNearestTiles.push(this.board.tiles[x - 1][y]);
+    }
+    return arrayOfNearestTiles;
+  };
 }
